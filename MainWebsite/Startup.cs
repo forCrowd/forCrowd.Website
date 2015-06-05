27f085992162405530efd2a0be2d30c35d1d0fd8
coni2k <serkanholat@hotmail.com>
2015-06-05 16:09:44 +0300

    0.1

---
+++ MainWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MainWebsite.Startup))]
+namespace MainWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
