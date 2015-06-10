97bae3875bb4143ffccf219bbf0a09466d179c31
coni2k <serkanholat@hotmail.com>
2015-06-10 18:31:07 +0300

    0.1

--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("forCrowd.org Website")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("forCrowd.org")]
+[assembly: AssemblyDescription("Website of forCrowd Foundation; http://forcrowd.org")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("forCrowd Foundation")]
-[assembly: AssemblyProduct("forcrowd.org")]
+[assembly: AssemblyProduct("forCrowd.org")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
